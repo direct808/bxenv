1c6c34ab3abc5fdfcf6bcba3078275f9d27216c3
direct808 <direct808@yandex.ru>
2016-09-07 10:09:44 +0300

    fix

--- bin/bxenv.js
+++ bin/bxenv.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-require('../index');
+var bxenv = require('commander');
+var pkg = require('../package');
+var init = require('../lib/init');
+var start = require('../lib/start');
+var sendmail = require('../lib/sendmail');
+
+bxenv.version(pkg.version)
+
+bxenv.command('init [php_version]')
+    .description('Initialize config and database files in work directory')
+    .action(init);
+
+bxenv.command('start')
+    .description('Initialize config and database files in work directory')
+    .action(start);
+
+bxenv.command('sendmail')
+    .description('sendmail for php')
+    .action(sendmail);
+
+
+bxenv.parse(process.argv);
+
+//require('../index');
